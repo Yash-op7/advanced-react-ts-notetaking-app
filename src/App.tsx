b64562874608c9001f99456a36b71d41aba265f1
yash-op7 <yashm3375@gmail.com>
2024-10-24 04:14:51 +0530

    finished setting up the router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {Routes, Route, Navigate} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+   <Container className='my-4'>
+     <Routes>
+      <Route path='/' element={<div>Home page</div>} />
+      <Route path='/new' element={<h1>New</h1>} />
+      <Route path='/:id'>
+        <Route index element={<h1>Yokoso</h1>} />
+        <Route path='edit' element={<h1>edit</h1>} />
+      </Route>
+      <Route path='/*' element={<Navigate to='/' />} />
+    </Routes>
+   </Container>
   )
 }
 
