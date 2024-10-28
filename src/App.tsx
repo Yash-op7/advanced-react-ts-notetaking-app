3c7c65ff9ae4dc4b4042afe954bedf15dea9ed57
yash-op7 <yashm3375@gmail.com>
2024-10-28 08:32:59 +0530

    Hiatus, currently working on TopTutors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Routes, Route, Navigate} from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import { NewNote } from './NewNote';
 
 function App() {
   return (
    <Container className='my-4'>
      <Routes>
       <Route path='/' element={<div>Home page</div>} />
-      <Route path='/new' element={<h1>New</h1>} />
+      <Route path='/new' element={<NewNote />} />
       <Route path='/:id'>
         <Route index element={<h1>Yokoso</h1>} />
         <Route path='edit' element={<h1>edit</h1>} />
